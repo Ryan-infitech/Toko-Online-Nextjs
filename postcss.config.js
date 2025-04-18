22046a7bababbf65787775ae2c93721057a9a60f
Rian Septiawan <rianseptiawan@infitech.or.id>
2025-04-18 23:55:30 +0700

    init

---
+++ postcss.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+    "postcss-preset-env": {
+      features: {
+        "nesting-rules": true,
+      },
+    },
+    ...(process.env.NODE_ENV === "production"
+      ? { cssnano: { preset: "default" } }
+      : {}),
+  },
+};
